bf7c71aa001944f3634b6e41c67ec672d7abd528
Saurabh Agrawal <saurabhdcagrawal@gmail.com>
2022-01-22 20:49:41 -0500

    updates with Merge Sort

--- LeetCode/src/main/java/SearchSort.java
+++ LeetCode/src/main/java/SearchSort.java
@@ -1,70 +1,64 @@
 package main.java;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class SearchSort {
- //Compare consecutive pairs
- //starting from first element, compare with the next element, swap the pairs if the element at higher index
- //is less than the element at lower index
+    //Compare consecutive pairs
+    //starting from first element, compare with the next element, swap the pairs if the element at higher index
+    //is less than the element at lower index
 //first pass maximum element comes to end, so in next iteration we can discount comparing the last element
     //Similarly for others
-    public static int[] bubbleSort(int[] arr){
-        int n= arr.length;
-        for(int i=0; i<n-1;i++) {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
             //in an array of n elements, maximum swaps and comparisons can be n-1;
             //Hence outer loop is bound by n-1
-            for (int j=0;j<n-i-1;j++){
-                if(arr[j+1]<arr[j]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            System.out.println("Pass "+ (i+1)+" "+Arrays.toString(arr));
+            System.out.println("Pass " + (i + 1) + " " + Arrays.toString(arr));
         }
-
-      return arr;
     }
+
     //Best case complexity is O(n) if array is already sorted
-    public static int[] bubbleSortOptimized(int[] arr){
-        int n= arr.length;
-        boolean isSwap=true;
-        for(int i=0; i<n-1&&isSwap;i++) {
-            isSwap=false;
-            for (int j=0;j<n-i-1;j++){
-                if(arr[j+1]<arr[j]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                    isSwap=true;
+    public static void bubbleSortOptimized(int[] arr) {
+        int n = arr.length;
+        boolean isSwap = true;
+        for (int i = 0; i < n - 1 && isSwap; i++) {
+            isSwap = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    isSwap = true;
                 }
             }
-            System.out.println("Pass "+ i+" "+Arrays.toString(arr));
+            System.out.println("Pass " + i + " " + Arrays.toString(arr));
         }
-
-        return arr;
     }
+
     //Find the position of the minimum element, swap its position with first element
     //Then find the position of second minimum element , swap it with 2nd element and so on and so forth
-    public static int[] selectionSort(int[] arr){
-        int n= arr.length;
-        for(int i=0; i<n-1;i++) {
-            int min_index=i;
-            for (int j=i+1;j<n;j++){
-                if(arr[j]<arr[min_index])
-                    min_index=j;
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_index])
+                    min_index = j;
             }
-            int temp=arr[i];
-            arr[i]=arr[min_index];
-            arr[min_index]=temp;
-            System.out.println("Pass "+ i+" "+Arrays.toString(arr));
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+            System.out.println("Pass " + i + " " + Arrays.toString(arr));
         }
-
-        return arr;
     }
+
     //{45,//2,34,56,8}
     //Right shift the cards from 0 to i-1 and place the key on left
     // 1: Iterate from arr[1] to arr[n] over the array.
@@ -72,122 +66,222 @@ public class SearchSort {
     //3: If the key element is smaller than its predecessor
     // Move the greater elements one position to right to make space for the key...
     //place the key on its position to left in the end
-    public static int[] insertionSort(int[] arr){
+    public static void insertionSort(int[] arr) {
         System.out.println("Original array");
         System.out.println(Arrays.toString(arr));
-        int n= arr.length;
-        for(int i=1; i<n; i++) {
-            int key=arr[i];
-            int j=i-1;
-            System.out.println("Key is " +key);
-            System.out.println("Predecessor is " +arr[j]);
-            while(j>=0 && key<arr[j]){
-                arr[j+1]=arr[j];
-                j=j-1;
-                System.out.println("Shifting for pass "+i+" "+Arrays.toString(arr));
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            System.out.println("Key is " + key);
+            System.out.println("Predecessor is " + arr[j]);
+            while (j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                System.out.println("Shifting for pass " + i + " " + Arrays.toString(arr));
             }
-            arr[j+1]=key;
-            System.out.println("Pass "+i+" "+Arrays.toString(arr));        }
-        return arr;
-    }
-    //Counting sort
-    public static void countingSort(int[] arr){
-        System.out.println("Original Array"+Arrays.toString(arr));
-        int largest=arr[0];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>largest)
-                largest=arr[i];
+            arr[j + 1] = key;
+            System.out.println("Pass " + i + " " + Arrays.toString(arr));
         }
-        int[] count_arr= new int[largest+1];
-        for(int i=0;i<arr.length;i++)
+    }
+
+    //Counting sort
+    public static void countingSort(int[] arr) {
+        System.out.println("Original Array" + Arrays.toString(arr));
+        int largest = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest)
+                largest = arr[i];
+        }
+        int[] count_arr = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++)
             count_arr[arr[i]]++;
-        System.out.println("Count array"+Arrays.toString(count_arr));
-        for(int i=1;i<count_arr.length;i++)
-            count_arr[i]+=count_arr[i-1];
-        System.out.println("Count array after cumulative sum"+Arrays.toString(count_arr));
-        int[] sorted_arr=new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            sorted_arr[count_arr[arr[i]]-1]=arr[i];
+        System.out.println("Count array" + Arrays.toString(count_arr));
+        for (int i = 1; i < count_arr.length; i++)
+            count_arr[i] += count_arr[i - 1];
+        System.out.println("Count array after cumulative sum" + Arrays.toString(count_arr));
+        int[] sorted_arr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            sorted_arr[count_arr[arr[i]] - 1] = arr[i];
             System.out.println(Arrays.toString(sorted_arr));
             count_arr[arr[i]]--;
         }
-        for(int i=0;i<arr.length;i++)
-            arr[i]=sorted_arr[i];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = sorted_arr[i];
     }
-    public static void bucketSort(float[] arr, int bucketSize){
-        System.out.println("Original Array"+Arrays.toString(arr));
+
+    public static void bucketSort(float[] arr, int bucketSize) {
+        System.out.println("Original Array" + Arrays.toString(arr));
 
         //Create buckets of arraylist
         @SuppressWarnings("unchecked")
-        ArrayList<Float>[] buckets= new ArrayList[bucketSize];
+        ArrayList<Float>[] buckets = new ArrayList[bucketSize];
 
         // Find bucket for array, multiply by bucket size, take floor value,
         //once you go to index go to the bucket and add the element
-         for(int i=0; i<arr.length;i++){
-            int bucketIndex= (int)(arr[i]* bucketSize);
-            if(buckets[bucketIndex]==null){
-                ArrayList<Float> bucketList= new ArrayList<>();
-                buckets[bucketIndex]=bucketList;
-         }
+        for (int i = 0; i < arr.length; i++) {
+            int bucketIndex = (int) (arr[i] * bucketSize);
+            if (buckets[bucketIndex] == null) {
+                ArrayList<Float> bucketList = new ArrayList<>();
+                buckets[bucketIndex] = bucketList;
+            }
             buckets[bucketIndex].add(arr[i]);
         }
-         //Sort individual buckets using stable sort.. Quicksort
-        for(int i=0; i<bucketSize;i++) {
-            if(buckets[i]!=null)
+        //Sort individual buckets using stable sort.. Quicksort
+        for (int i = 0; i < bucketSize; i++) {
+            if (buckets[i] != null)
                 Collections.sort(buckets[i]);
         }
-         //Now navigate through each bucket and element one by one to original array
-        int index=0;
-        for(int i=0; i<bucketSize;i++)
-            if(buckets[i]!=null){
-                for(int j=0;j<buckets[i].size();j++){
-                 arr[index]=buckets[i].get(j);
-                 index++;
+        //Now navigate through each bucket and element one by one to original array
+        int index = 0;
+        for (int i = 0; i < bucketSize; i++)
+            if (buckets[i] != null) {
+                for (int j = 0; j < buckets[i].size(); j++) {
+                    arr[index] = buckets[i].get(j);
+                    index++;
                 }
+            }
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
         }
     }
 
-    public static int[] mergeSort(int[] arr){
-        int n= arr.length;
-        for(int i=0; i<n; i++) {
-            int key=arr[i];
-            int index=i+1;
-           // while()
+    public static void merge(int[] arr, int low, int mid, int high) {
+        System.out.println("Low " + low + " mid " + mid + " high " + high);
+        int n1 = mid - low + 1;
+        int n2 = high - (mid + 1) + 1;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            left[i] = arr[low + i];
+        System.out.println("Left array " + Arrays.toString(left));
+        for (int j = 0; j < n2; j++)
+            right[j] = arr[mid + 1 + j];
+        System.out.println("Right array " + Arrays.toString(right));
+
+        int p = 0;
+        int q = 0;
+        int k = low;
+        while (p < n1 && q < n2) {
+            if (left[p] < right[q]) {
+                arr[k] = left[p];
+                p++;
+            } else {
+                arr[k] = right[q];
+                q++;
+            }
+            k++;
+        }
+        while (p < n1) {
+            arr[k] = left[p];
+            p++;
+            k++;
         }
 
-        return arr;
+        while (q < n2) {
+            arr[k] = right[q];
+            q++;
+            k++;
+        }
+
+        System.out.println("After merge " + Arrays.toString(arr));
     }
 
-    public static void main(String args[]){
+    public static void mergeSort(int[] arr) {
+        System.out.println("original array is " + Arrays.toString(arr));
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    //    Print1[1, 2, 15, 25, 40, 65, 0, 0, 0, 0]
+//Array B[1, 12, 30, 42, 100]
+    //Start from end because if you start from beginning you have to shift the array contents
+    public static void sortedMerge(int[] arrA, int[] arrB, int lengthA) {
+        int i = lengthA - 1;
+        int j = arrB.length - 1;
+        int k = lengthA+arrB.length-1;
+        while (i >= 0 && j >= 0) {
+            if (arrA[i] > arrB[j]) {
+                arrA[k] = arrA[i];
+                i--;
+            } else {
+                arrA[k] = arrB[j];
+                j--;
+            }
+            k--;
+        }
+        while (j >= 0) {
+            arrA[k] = arrB[j];
+            k--;
+            j--;
+        }
+
+
+    }
+    //(lips,cat,slip,mate,hello,tame)
+    public static void AnagramSort(String[] a){
+        Arrays.sort(a,new AnagramCustomComparator());
+
+    }
+    //to convert from char arr to string use new str
+    private static class AnagramCustomComparator implements Comparator<String>{
+        @Override
+        public int compare(String s1, String s2) {
+        char[] charS1=s1.toCharArray();
+        char[] charS2=s2.toCharArray();
+        Arrays.sort(charS1);
+        Arrays.sort(charS2);
+        return (new String(charS1).compareTo(new String(charS2)));
+        }
+    }
+
+    /*External sort 900 MB 0f data on disk..
+    100 MB of memory available
+    Divide into 9 chunks of 100 MB, bring chunk in memory everytime, sort the chunk and write back to disk;
+    Read 10 MB for every sorted chunk (call it i/p buffer) 9 chunks total of 90 MB,
+    and allocate 10 MB for output buffer
+    Do a 9 (K) way merge and store the results in output buffer
+    if output buffer is full, write back to disk or sorted file
+    When any of i/p buffer is empty, fill it with next 10 MB of data
+    and when its empty mark as exhausted and do not use it for merge
+*/
+
+        public static void main(String args[]) {
         System.out.println("***************************************");
         System.out.println("Bubble sort");
-        int[] arr1= {25,2,34,56,8};
+        int[] arr1 = {25, 2, 34, 56, 8};
         SearchSort.bubbleSort(arr1);
         System.out.println("Final sorted array");
         System.out.println(Arrays.toString(arr1));
         System.out.println("***************************************");
         System.out.println("Bubble sort optimized");
-        int[] arr2= {25,2,34,56,8};
+        int[] arr2 = {25, 2, 34, 56, 8};
         SearchSort.bubbleSortOptimized(arr2);
         System.out.println("Final sorted array");
         System.out.println(Arrays.toString(arr2));
         System.out.println("***************************************");
         System.out.println("Selection Sort");
-        int[] arr3= {25,2,34,56,8};
+        int[] arr3 = {25, 2, 34, 56, 8};
         SearchSort.selectionSort(arr3);
         System.out.println("Final sorted array");
         System.out.println(Arrays.toString(arr3));
         System.out.println("***************************************");
 
         System.out.println("Insertion Sort");
-        int[] arr4= {45,2,34,56,8};
+        int[] arr4 = {45, 2, 34, 56, 8};
         SearchSort.insertionSort(arr4);
         System.out.println("Final sorted array");
         System.out.println(Arrays.toString(arr4));
         System.out.println("***************************************");
 
         System.out.println("Counting Sort");
-        int[] arr5= {45,2,34,56,8};
+        int[] arr5 = {45, 2, 34, 56, 8};
         SearchSort.countingSort(arr5);
         System.out.println("Final sorted array");
         System.out.println(Arrays.toString(arr5));
@@ -195,13 +289,38 @@ public class SearchSort {
         System.out.println("***************************************");
 
         System.out.println("Bucket Sort");
-        float[] arr6= {0.42f,0.32f,0.23f,0.52f,0.25f,0.47f,0.51f};
-        SearchSort.bucketSort(arr6,10);
+        float[] arr6 = {0.42f, 0.32f, 0.23f, 0.52f, 0.25f, 0.47f, 0.51f};
+        SearchSort.bucketSort(arr6, 10);
         System.out.println("Final sorted array");
         System.out.println(Arrays.toString(arr6));
+
+        System.out.println("***************************************");
+        System.out.println("Merge Sort");
+        int[] arr7 = {45, 2, 56, 34, 8};
+        SearchSort.mergeSort(arr7);
+        System.out.println("Final sorted array");
+        System.out.println(Arrays.toString(arr7));
+
+        System.out.println("***************************************");
+        System.out.println("Sorted merge problem");
+        int[] arrA = {2, 15, 25, 40, 65, 0, 0, 0, 0, 0};
+        int[] arrB = {1, 12, 30, 42, 100};
+        System.out.println("Array A" + Arrays.toString(arrA));
+        System.out.println("Array B" + Arrays.toString(arrB));
+        SearchSort.sortedMerge(arrA, arrB, 5);
+        System.out.println("Final sorted array");
+        System.out.println(Arrays.toString(arrA));
+
+
+        System.out.println("***************************************");
+        System.out.println("Anagram sort problem");
+        String[] a={"lips","cat","slip","mate","hello","tame"};
+        System.out.println("Original array"+Arrays.toString(a));
+        SearchSort.AnagramSort(a);
+        System.out.println("Sorted with custom"+Arrays.toString(a));
+
+
     }
-
-
 
 
 }
