7ce80340372d2baaf0a51918d8f3d05072d644fb
Saurabh Agrawal <saurabhdcagrawal@gmail.com>
2022-02-07 14:29:37 -0500

    updates

--- LeetCode/src/main/java/datastructures/DPAndRecursion.java
+++ LeetCode/src/main/java/datastructures/DPAndRecursion.java
@@ -3,9 +3,18 @@ package main.java.datastructures;
 import main.java.util.GeneralUtility;
 
 import java.util.*;
-
-//top down dynamic programming memoization
+//Recursion
+//a problem built of sub problems is a recursive problem
+//Find the first n, write an algorithm to compute the nth etc
+//1)bottom up->solve the problem for a simple case, then for 2 elements, 3 elements
+//2)top down  --divide the problem into N sub problems, easy to think but can be complex
+//3)Half and half approach
+//Recursion is expensive,each recursive problem creates a call for stack, so uses at least O(n) memory
+//Recursive problems can be implemented iteratively, however it can be complex
+// top down DP memoization
 //bottom up as dp only
+//whenever cache involved its DP
+//For DP, find overlapping sub-problems, cache them for future recursive calls
 //every recursive problem has an iterative way
 //LCA
 
@@ -20,17 +29,12 @@ public class DPAndRecursion {
     }
 
 
-
-
     public static int fibonacciMemoization(int n){
        int [] memo = new int [n+1];
       return  fibonacciMemoization(n,memo);
     }
 
-
-
-
-
+    //n+1 because fib[n+1]it gives the answer
     public static int fibonacciMemoization(int n ,int[] memo){
      if (n==0 || n==1)  return n;
      //strong logic
@@ -60,6 +64,155 @@ public class DPAndRecursion {
         return c;
     }
 
+    //robot grid example [m+1][n+1] matrix... m,n will have the answer, origin point is (1,1)
+    public static int uniquePathsRecursion(int m, int n) {
+        if(m==1 || n==1)
+            return 1;
+        else
+            return uniquePathsRecursion(m-1,n)+uniquePathsRecursion(m,n-1);
+    }
+
+    //(m,n) denote the number of ways to reach a particular cell
+    public static int uniquePathsDP(int m, int n) {
+            int [][] total = new int[m][n];
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(i==0 || j==0)
+                        total[i][j]=1;
+                    else
+                        total[i][j]=total[i-1][j]+total[i][j-1];
+
+                    }
+            }
+            return total[m-1][n-1];
+        }
+
+    /*public static int uniquePathsExp(int m, int n) {
+        if(m<0||n<0)
+            return 0;
+        if (m == 1 && n == 1) {
+            return 1;
+        }
+        return uniquePathsExp(m - 1, n) + uniquePathsExp(m, n - 1);
+    }*/
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row=obstacleGrid.length-1;
+        int col=obstacleGrid[0].length-1;
+        if (obstacleGrid[0][0]==1||obstacleGrid[row][col]==1)
+            return 0;
+        return uniquePathsWithObstacles(obstacleGrid,row,col);
+
+    }
+
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid,int m, int n){
+        if(m<0 || n<0)
+            return 0;
+        else if(obstacleGrid[m][n]==1)
+            return 0;
+        else if (m==0 && n==0 )
+            return 1;
+        else
+            return uniquePathsWithObstacles(obstacleGrid,m-1,n)+uniquePathsWithObstacles(obstacleGrid,m,n-1);
+
+    }
+       //memoization will give you the DP matrix
+        public static int uniquePathsWithObstaclesMemoization(int[][] obstacleGrid) {
+            int row=obstacleGrid.length;
+            int col=obstacleGrid[0].length;
+            if (obstacleGrid[0][0]==1||obstacleGrid[row-1][col-1]==1)
+                return 0;
+            int[][] paths = new int[row][col];
+            return uniquePathsWithObstaclesMemoization(obstacleGrid,paths,row-1,col-1);
+
+        }
+
+        public static int uniquePathsWithObstaclesMemoization(int[][] obstacleGrid,int[][] paths,int m, int n){
+            if (m<0||n<0)
+                return 0;
+            else if (obstacleGrid[m][n]==1)
+                return 0;
+            else if(m==0&&n==0)
+                return 1;
+            else if(paths[m][n]==0)
+                paths[m][n]=uniquePathsWithObstaclesMemoization(obstacleGrid,paths,m-1,n)+
+                        uniquePathsWithObstaclesMemoization(obstacleGrid,paths,m,n-1);
+           System.out.println("m" +m +" "+" n"+ n+ " "+paths[m][n]);
+            return paths[m][n];
+
+        }
+// You could also modify the obstacle grid to solve this question
+    public int uniquePathsWithObstaclesDP(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        if (obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1)
+            return 0;
+        int[][] paths = new int[m][n];
+        paths[0][0]=1;
+        for(int i=1;i<m;i++){
+            if(obstacleGrid[i][0]==1)
+                paths[i][0]=0;
+            else
+                paths[i][0]=paths[i-1][0];
+        }
+
+        for(int j=1;j<n;j++){
+            if(obstacleGrid[0][j]==1)
+                paths[0][j]=0;
+            else
+                paths[0][j]=paths[0][j-1];
+        }
+
+        for(int i=1; i<m;i++){
+            for(int j=1;j<n;j++){
+                if (obstacleGrid[i][j]==1)
+                    paths[i][j]=0;
+                else
+                    paths[i][j]=paths[i-1][j]+paths[i][j-1];
+            }
+        }
+        return paths[m-1][n-1];
+    }
+
+    //start with highest denomination...
+    //denoms={25,10,5,1}
+    public static int coinProblemRecurse(int n, int[] denoms){
+        return coinProblemRecurse(n,denoms,0);
+    }
+    public static int coinProblemRecurse(int amount, int[] denoms,int index){
+       //reduction
+        if(index>=denoms.length-1)
+            return 1;
+        int coin=denoms[index];
+        int ways=0;
+        for(int i=0; i*coin<=amount;i++) {
+            int remainingAmount = amount - i * coin;
+            // System.out.println("Calling for "+remainingAmount +" and index "+(index+1));
+            ways += coinProblemRecurse(remainingAmount, denoms, index+1);
+        }
+        return ways;
+    }
+
+    public static int coinProblemDP(int n, int[] denoms){
+        int[][] map= new int[n+1][denoms.length+1];
+        return coinProblemDP(n,denoms,map,0);
+    }
+
+    public static int coinProblemDP(int amount, int[] denoms,int[][]map,int index){
+        if(map[amount][index]>0)
+            return map[amount][index];
+        if(index>=denoms.length-1)
+            return 1;
+        int coin=denoms[index];
+        int ways=0;
+        for(int i=0; i*coin<=amount;i++) {
+            int remainingAmount = amount - i * coin;
+            ways += coinProblemDP(remainingAmount, denoms,map, index+1);
+        }
+        map[amount][index]=ways;
+        //printMatrix(map);
+        return map[amount][index];
+    }
+
     public static int countNoOfWaysSteps(int n){
        if(n<0)
            return 0;
@@ -69,6 +222,20 @@ public class DPAndRecursion {
      return countNoOfWaysSteps(n-1)+countNoOfWaysSteps(n-2)+countNoOfWaysSteps(n-3);
 //put condition in function and check
     }
+    public static int countNoOfWaysStepsMemoization(int n){
+       int [] memo = new int[n+1];
+       return countNoOfWaysStepsMemoization(n,memo);
+    }
+    public static int countNoOfWaysStepsMemoization(int n, int[] memo){
+        if(n<0) return 0;
+        else if(n==0) return 1;
+
+        if(memo[n]==0)
+            memo[n]=countNoOfWaysStepsMemoization(n-1,memo)+countNoOfWaysStepsMemoization(n-2,memo)+countNoOfWaysStepsMemoization(n-3,memo);
+
+        return memo[n];
+    }
+
 
     public static int rob(int[] nums) {
         int[] amt = new int[nums.length+1] ;
@@ -152,6 +319,14 @@ public class DPAndRecursion {
        }
 
 
+    public static void printMatrix(int[][]matrix){
+        for (int i=0;i<matrix.length;i++){
+            //System.out.println();
+            for (int j=0; j <matrix[0].length;j++)
+                System.out.print(matrix[i][j]+ " ");
+            System.out.println("");
+        }
+    }
 
 
 
@@ -273,23 +448,8 @@ public class DPAndRecursion {
       return permutations;
     }
 
-//
-    public int uniquePaths(int m, int n) {
-        if(m==1 ||n==1) return 1;
-        int [][] total = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0)
-                    total[i][j]=1;
-                else
-                    total[i][j]=total[i-1][j]+total[i][j-1];
 
-            }
 
-        }
-
-        return total[m-1][n-1];
-    }
 
      public static String getNewWord(int position ,String word ,char c) {
          String head = null, tail = null;
@@ -318,12 +478,28 @@ public class DPAndRecursion {
         System.out.println("Longest non repeating subsequence");
         System.out.println(longestNonRepeatingSubstring("bbbb"));
         System.out.println(lengthOfLongestSubstring("abcbda"));
-        System.out.println("Edit distance "+editDistance("ISHA","SAURABH"));
+        System.out.println("Edit distance "+editDistance("TASH","SAURABH"));
         System.out.println("Permutations");
         System.out.println(getPermutations("abc"));
         System.out.println("Integer Permutations");
         int[] perm_arr={1,2,3};
         System.out.println(permute(perm_arr,0));
+        System.out.println("No of ways steps");
+        System.out.println(countNoOfWaysSteps(20));
+        System.out.println(countNoOfWaysStepsMemoization(20));
+        System.out.println("No of unique paths");
+        System.out.println(uniquePathsDP(23,12));
+        System.out.println(uniquePathsRecursion(23,12));
+        //System.out.println(uniquePathsExp(23,12));
+        System.out.println("No of unique paths with obstacles");
+        int [][] obstaclePath={{0,0,0},{0,1,0},{0,0,0}};
+        System.out.println(uniquePathsWithObstaclesMemoization(obstaclePath));
+
+        System.out.println("Coin problem");
+        int [] denoms={25,10,5,1};
+        System.out.println(coinProblemRecurse(50,denoms));
+        System.out.println("Coin problem DP");
+        System.out.println(coinProblemDP(50,denoms));
     }
 
 
